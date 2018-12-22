79d784fedfb294b6b94145e7d40fb9a1c2c64048
DKorchev <korchev.d@gmail.com>
2018-12-22 20:03:43 +0200

    22.12.18 final

--- app/src/main/java/com/example/admin/justjava/MainActivity.java
+++ app/src/main/java/com/example/admin/justjava/MainActivity.java
@@ -3,12 +3,11 @@ package com.example.admin.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 //import java.text.NumberFormat;
-/**
- * TESTING GITHUB!!!
- */
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -25,19 +24,26 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String message = "The price is " + calculatePrice() + ", man!";
-        message += " Thanks. Love you!";
-        displayMessage(message);
+        boolean hasWhippedCream = ((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked();
+
+        String priceMessage = createOrderSummaryMessage(hasWhippedCream);
+        displayMessage(priceMessage);
     }
 
     /**
      * Calculates the price of the order.
-     *
-     * @param
+     * @param hasWhipperCream is true when user want to add whipped cream to the order
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean hasWhipperCream) {
+        int base = 5;
+        if (hasWhipperCream == true)
+            base += 1;
+        return quantity * base;
     }
+
+    /**
+     * This method is call when plus button is clicked
+     */
 
     public void increment(View view) {
         if (quantity < 100) {
@@ -46,13 +52,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    /**
+     * This method is call when minus button is clicked
+     */
     public void dicrement(View view) {
         if (quantity >= 2) {
             quantity = quantity - 1;
             displayQuantity(quantity);
         }
 
+    }
+
+    /**
+     * This method create the message, that will be displayed on the screen when order button is clicked.
+     * calculatePrice(hasWhippedCream) method called for order price
+     */
+
+    private String createOrderSummaryMessage(boolean hasWhippedCream) {
+        String priceMessage = "Add whipped cream: " + hasWhippedCream + "\nThe price is " + calculatePrice(hasWhippedCream) + ", man!";
+        return priceMessage;
     }
 
     /**
