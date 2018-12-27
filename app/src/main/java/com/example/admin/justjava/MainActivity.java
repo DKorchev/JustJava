27c2db326207db8f6036fc0b0ddbfc0cd64e4efd
DKorchev <korchev.d@gmail.com>
2018-12-27 19:23:47 +0200

    with email

--- app/src/main/java/com/example/admin/justjava/MainActivity.java
+++ app/src/main/java/com/example/admin/justjava/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.admin.justjava;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
 
 //import java.text.NumberFormat;
 
@@ -24,11 +31,29 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText enterName = findViewById(R.id.textPersonName);
+        String Name = enterName.getText().toString();
+
         boolean hasWhippedCream = ((CheckBox) findViewById(R.id.whipped_cream_checkbox)).isChecked();
         boolean hasChocolate = ((CheckBox) findViewById(R.id.chocolate_checkbox)).isChecked();
+        String priceMessage = createOrderSummaryMessage(hasWhippedCream, hasChocolate, Name);
 
-        String priceMessage = createOrderSummaryMessage(hasWhippedCream, hasChocolate);
-        displayMessage(priceMessage);
+        String subject = "JustJava order for " + Name;
+        String sendto = "admin@adeya.com.ua";
+        composeEmail(subject, priceMessage, sendto);
+
+        //displayMessage(priceMessage);
+    }
+
+    public void composeEmail(String subject, String body, String sendto) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{sendto});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -42,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         if (hasWhipperCream)
             base += 1;
         if (hasChocolate)
-            base += 1;
+            base += 2;
         return quantity * base;
     }
 
@@ -54,7 +79,9 @@ public class MainActivity extends AppCompatActivity {
         if (quantity < 100) {
             quantity = quantity + 1;
             displayQuantity(quantity);
-        }
+        } else
+            //this will show short message (Toast), when user try to dicrement number of cofees below 1
+            Toast.makeText(getApplicationContext(), "You can order maximum 100 cups of coffee", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -64,8 +91,9 @@ public class MainActivity extends AppCompatActivity {
         if (quantity >= 2) {
             quantity = quantity - 1;
             displayQuantity(quantity);
-        }
-
+        } else
+            //this will show short message (Toast), when user try to dicrement number of cofees below 1
+            Toast.makeText(getApplicationContext(), "You can order minimum 1 cup of coffee", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -73,8 +101,13 @@ public class MainActivity extends AppCompatActivity {
      * calculatePrice(hasWhippedCream, hasChocolate) method called for order price
      */
 
-    private String createOrderSummaryMessage(boolean hasWhippedCream, boolean hasChocolate) {
-        String priceMessage = "Add whipped cream: " + hasWhippedCream + "\nAdd chocolate:" + hasChocolate + "\nThe price is " + calculatePrice(hasWhippedCream, hasChocolate) + ", man!";
+    private String createOrderSummaryMessage(boolean hasWhippedCream, boolean hasChocolate, String Name) {
+        String priceMessage = "Name: " + Name;
+        priceMessage += "\nAdd whipped cream: " + hasWhippedCream;
+        priceMessage += "\nAdd chocolate: " + hasChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nThe price is: " + calculatePrice(hasWhippedCream, hasChocolate);
+        priceMessage += "\nThank you!";
         return priceMessage;
     }
 
